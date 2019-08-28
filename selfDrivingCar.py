dab5680a71de740446cdcfaf7b777c7570eca7b3
Antarip Saha <52539087+AntaripSaha@users.noreply.github.com>
2019-08-28 22:23:46 +0600

    Update selfDrivingCar.py

--- selfDrivingCar.py
+++ selfDrivingCar.py
@@ -17,10 +17,22 @@ def canny_filter (image):
     #cv.waitKey(0)
     return canny
 
+# here in this function we will identify the required places from a picture.
+def region_of_interst (image):
+    height = image.shape[0] #the value of Y
+    polygons = np.array([
+        [(200, height),  (1100, height)  , (550 , 250)]
+    ]) #the traingle we need
+    mask = np.zeros_like(image) #this func will make the value of pixels zero means black.
+    cv.fillPoly(mask, polygons ,255) # now it will take the mask and the polygons then,
+    # it will make the values for polygons area pixels is 255. so the site will be white.
+    return mask
+
+
+
 
 # normally reading a image from dataset
-img = cv.imread('E:\Thesis\DataSet\Desertroad.jpg')
-
+img = cv.imread('DataSet/tti.jpg')
 # need to copy the image because
 # if main image change then there will be problem in next steps
 lane_img = np.copy(img)
@@ -28,11 +40,10 @@ lane_img = np.copy(img)
 # calling canny filter function
 canny = canny_filter(lane_img)
 # printing the image in matplot, or we can say as with height and width (x,y)
-#cv.imshow('result',canny)
+cv.imshow('result', region_of_interst(canny))
 
 plt.imshow(canny)
 
 # cv.waitkey(0) it's not nessasary..
-#cv.waitKey(0)
+cv.waitKey(0)
 plt.show()
-
